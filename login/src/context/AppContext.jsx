6f1b81eb903eacb987341d91ad5406a81b6bdf16
Shobhit Sharma <165416341+shobhit7098@users.noreply.github.com>
2026-02-19 07:47:16 +0530

    Update backend URL in AppContext

--- login/src/context/AppContext.jsx
+++ login/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const backendUrl =
-    import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+    import.meta.env.VITE_BACKEND_URL || "https://login-2-zbeb.onrender.com/";
 
   // Token state
   const [token, setToken] = useState(
